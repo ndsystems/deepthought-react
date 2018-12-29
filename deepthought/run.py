769ea2de92688c2f11755ff6c32f5711e896508a
Kesavan PS <pskesavan@tifrh.res.in>
2018-12-29 13:53:45 +0530

    added a load microscope button and related functions, changed variable conventions, relocated run

--- deepthought/run.py
+++ deepthought/run.py
@@ -1,5 +1,6 @@
 from PyQt5 import QtWidgets
-from main import Ui_Form
+from gui import main
+import controls
 import sys
 
 
@@ -7,30 +8,36 @@ class mainwindow(QtWidgets.QMainWindow):
     def __init__(self):
         super(mainwindow, self).__init__()
         self.Form = QtWidgets.QWidget()
-        self.ui = Ui_Form()
+        self.ui = main.Ui_Form()
         self.ui.setupUi(self.Form)
-
+        self.mmc = None
         self.epiShutter = 0
         self.diaShutter = 0
-        self.ui.EpiToggleBtn.clicked.connect(self.epi_toggle)
-        self.ui.DiaToggleBtn.clicked.connect(self.dia_toggle)
+        self.ui.epiToggleBtn.clicked.connect(self.epi_toggle)
+        self.ui.diaToggleBtn.clicked.connect(self.dia_toggle)
+
+    def load_microscope(self):
+        if self.mmc is None:
+            self.mmc = controls.loadDevices()
+        else:
+            pass
 
     def epi_toggle(self):
         if self.epiShutter is 0:
-            self.ui.EpiToggleBtn.setText("ON")
+            self.ui.epiToggleBtn.setText("ON")
             self.epiShutter = 1
 
         else:
-            self.ui.EpiToggleBtn.setText("EPI")
+            self.ui.epiToggleBtn.setText("EPI")
             self.epiShutter = 0
 
     def dia_toggle(self):
         if self.diaShutter is 0:
-            self.ui.DiaToggleBtn.setText("ON")
+            self.ui.diaToggleBtn.setText("ON")
             self.diaShutter = 1
 
         else:
-            self.ui.DiaToggleBtn.setText("DIA")
+            self.ui.diaToggleBtn.setText("DIA")
             self.diaShutter = 0
 
 
