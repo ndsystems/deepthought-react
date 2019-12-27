04d2303d33d521511f3883b3fd6e9aa475cd1823
Kesavan <pskesavan@tifrh.res.in>
2019-12-27 16:24:37 +0530

    simplified path, added unload

--- deepthought/hardware.py
+++ deepthought/hardware.py
@@ -1,26 +1,26 @@
 import MMCorePy
 import os
 
-mm2_path = ["C:", "Program Files", "Micro-Manager-2.0gamma"]
-mm2_path = os.path.join(*mm2_path)
+mm2_path = "C:\Program Files\Micro-Manager-2.0gamma"
 
 
 class Microscope():
     def __init__(self, config_path):
+        self.config_path = os.path.abspath(config_path)
         self.user_dir = os.getcwd()
 
     def load(self):
-        config_path = os.path.abspath(config_path)
         self.mmc = MMCorePy.CMMCore()
-        os.chdir(mm2_path)  # cwd to mm2 path for dlls to load
-        self.mmc.loadSystemConfiguration(config_path)
+        # cwd to mm2 path for dlls to load
+        os.chdir(mm2_path)
+        self.mmc.loadSystemConfiguration(self.config_path)
 
     def unload(self):
-        return
+        self.mmc.reset()
 
 
 if __name__ == "__main__":
-    scope = Microscope(os.path.join(*["config", "Bright_Star.cfg"])
+    scope = Microscope("configs\Bright_Star.cfg")
     scope.load()
     # ...
     scope.unload()
