ba4ee21374695d0113b586dd90cfdd0215604a80
Kesavan <pskesavan@tifrh.res.in>
2019-12-27 19:23:09 +0530

    contrast adjusted

--- deepthought/view.py
+++ deepthought/view.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 
-def dd(image):
-	plt.imshow(image)
-	plt.show()
-	plt.close()
 
+def dd(image):
+    plt.imshow(image, cmap="gray", clim=[100, 4095])
+    plt.show()
+    plt.close()
