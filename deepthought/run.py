6b56d88862f595230e37897eee2ea0cd8275bd4b
Kesavan <pskesavan@tifrh.res.in>
2019-12-29 19:44:26 +0530

    crude client working example

--- deepthought/run.py
+++ deepthought/run.py
@@ -4,17 +4,49 @@ import socket
 import pickle
 from contextlib import closing
 
+"""
+    def channel(self, channel):
+        self.mmc.setConfig("channel", channel)
+
+    def objective(self, objective):
+        self.mmc.setConfig("objective", objective)
+
+    def snap(self):
+        self.mmc.snapImage()
+        return self.mmc.getImage()
+
+    def moveZ(self, value):
+        self.mmc.setPosition(value)
+
+    def moveXY(self, x, y):
+        self.mmc.setXYPosition(x, y)
+"""
 
 
-def from_server(HOST, PORT):
-    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
-        s.connect((HOST, PORT))
-        s.sendall(b'Hello, world')
-        data = s.recv(1024)
-    return pickle.loads(data)
+class Connect:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def connect(self):
+        self.s.connect((self.host, self.port))
+
+    def send(self, message):
+        self.s.sendall(message)
+
+    def get(self):
+        received = str(self.s.recv(1024))
+        return received
+
+    def close(self):
+        self.s.close()
 
 
 # write a live program here.
-
 if __name__ == "__main__":
-    scope = from_server("127.0.0.1", 2500)
+    server = Connect("localhost", 2500)
+    server.connect()
+    server.send("""mmc.setConfig("channel", "BF")""")
+    # server.send("mmc.snapImage()")
+     # server.send("mmc.getImage()")
