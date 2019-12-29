9d030fd17caa3199a7b0b1c8086822a6dded8d11
Kesavan <pskesavan@tifrh.res.in>
2019-12-29 15:27:48 +0530

    updated for context manger

--- deepthought/run.py
+++ deepthought/run.py
@@ -2,15 +2,19 @@ from hardware import Microscope
 from view import dd
 import socket
 import pickle
+from contextlib import closing
+
 
 
 def from_server(HOST, PORT):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
         s.connect((HOST, PORT))
         s.sendall(b'Hello, world')
         data = s.recv(1024)
     return pickle.loads(data)
 
+
+# write a live program here.
 
 if __name__ == "__main__":
     scope = from_server("127.0.0.1", 2500)
