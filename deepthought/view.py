4b979255a2a34e7f2d5cd8949a05564bc8882b32
Kesavan Subburam <pskesavan@tifrh.res.in>
2020-04-28 23:49:32 +0530

    html frontend to display image, adjust stage settings

--- deepthought/view.py
+++ deepthought/view.py
@@ -1,20 +1,24 @@
 import matplotlib.pyplot as plt
 import mpld3
-from flask import Flask
+from flask import Flask, render_template, request
 from controller import AcquisitionControl
 
 app = Flask(__name__)
-
+scope = AcquisitionControl("localhost", 2500)
 
 @app.route('/')
 def index():
-    scope = AcquisitionControl("localhost", 2500)
     img = scope.image()
     fig, ax = plt.subplots()
     ax.imshow(img)
     html = mpld3.fig_to_html(fig)
-    return html
+    return render_template("index.html", mpl_figure=html)
 
+@app.route('/', methods=['POST'])
+def my_form_post():
+    z_value = float(request.form['z_value'])
+    scope.setPosition(z_value)
+    return '', 204
 
 if __name__ == "__main__":
     app.run()
