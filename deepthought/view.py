6fc6dc5c0dc581468c4935d7511a0a50dc01ee05
Kesavan Subburam <pskesavan@tifrh.res.in>
2020-05-01 06:31:56 +0530

    image objects sent through websockets. working code - take a timelapse/continuous image with js

--- deepthought/view.py
+++ deepthought/view.py
@@ -1,8 +1,10 @@
 import matplotlib.pyplot as plt
 import mpld3
+import json
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit, send
 from controller import AcquisitionControl
+import time
 
 app = Flask(__name__)
 socketio = SocketIO(app)
@@ -12,16 +14,19 @@ scope = AcquisitionControl("localhost", 2500)
 
 @app.route('/')
 def index():
+    return render_template("index.html")
+
+@socketio.on('image')
+def image_to_json(params):
     img = scope.image()
-    html = image_to_html(img)
-    return render_template("index.html", mpl_figure=html)
 
-
-def image_to_html(img):
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(dpi=150)
     ax.imshow(img, origin='lower', interpolation='nearest')
-    html = mpld3.fig_to_html(fig)
-    return html
+    ax.set_axis_off()
+
+    js_data = mpld3.fig_to_dict(fig)
+    emit("image", js_data)
+    time.sleep(0.5)
 
 
 @socketio.on('connect')
@@ -29,11 +34,12 @@ def hello():
     print("hello world")
 
 
+
 @socketio.on('stage')
 def stage_update(data):
     print(data)
     emit("updateState", data)
 
-
+#https://stackoverflow.com/questions/25069898/display-mpld3-chart-in-html-with-django
 if __name__ == "__main__":
     socketio.run(app)
