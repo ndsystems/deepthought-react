c7039efb32fea1677c6c47e5561b25f941862166
Kesavan Subburam <pskesavan@tifrh.res.in>
2020-05-02 15:25:03 +0530

    imshow now takes whole fig

--- deepthought/view.py
+++ deepthought/view.py
@@ -3,11 +3,13 @@ import mpld3
 import json
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit, send
-from controller import AcquisitionControl
+from handler import AcquisitionControl
 import time
+
 
 app = Flask(__name__)
 socketio = SocketIO(app)
+
 
 scope = AcquisitionControl("localhost", 2500)
 
@@ -16,17 +18,31 @@ scope = AcquisitionControl("localhost", 2500)
 def index():
     return render_template("index.html")
 
+
+def make_image(image, size=(5, 5), dpi=150):
+    fig = plt.figure()
+    fig.set_size_inches(size)
+    ax = plt.Axes(fig, [0., 0., 1., 1.])
+    ax.set_axis_off()
+    fig.add_axes(ax)
+    plt.set_cmap('gray')
+    ax.imshow(image, aspect='equal')
+    return fig
+
+
 @socketio.on('image')
 def image_to_json(params):
     img = scope.image()
 
     fig, ax = plt.subplots(dpi=150)
-    ax.imshow(img, origin='lower', interpolation='nearest')
+    ax.imshow(img, cmap="gray", origin='lower', interpolation='nearest')
     ax.set_axis_off()
 
     js_data = mpld3.fig_to_dict(fig)
+
+    plt.close()
     emit("image", js_data)
-    time.sleep(0.5)
+    time.sleep(0.1)
 
 
 @socketio.on('connect')
@@ -34,12 +50,14 @@ def hello():
     print("hello world")
 
 
-
 @socketio.on('stage')
 def stage_update(data):
     print(data)
     emit("updateState", data)
 
-#https://stackoverflow.com/questions/25069898/display-mpld3-chart-in-html-with-django
+
 if __name__ == "__main__":
-    socketio.run(app)
+    try:
+        socketio.run(app, host="0.0.0.0")
+    except ValueError:
+        pass
