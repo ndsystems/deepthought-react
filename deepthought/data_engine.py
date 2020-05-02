f34e468491c4bca8e7f0e7c01bab45c298035814
Kesavan Subburam <pskesavan@tifrh.res.in>
2020-05-02 15:40:43 +0530

    get all properties

--- deepthought/data_engine.py
+++ deepthought/data_engine.py
@@ -1,4 +1,5 @@
 from handler import BaseHandler
+from pprint import pprint
 
 
 class DeviceProperties(BaseHandler):
@@ -13,20 +14,24 @@ class DeviceProperties(BaseHandler):
     def get_current_exposure_time(self):
         return self.getExposure()
 
-    def get_loaded_devices(self):
-        devices = self.getLoadedDevices()
+    def get_all_properties(self):
+        list_of_devices = self.getLoadedDevices()
+        all_device_props = {}
 
-        data = {}
-        for device in devices:
-            properties = self.getDevicePropertyNames(device)
-            data[device] = {prop_name: None for prop_name in properties}
-        return data
+        for device in list_of_devices:
+            list_of_properties = self.getDevicePropertyNames(device)
+
+            device_props = {}
+            for property_ in list_of_properties:
+                value = self.getProperty(device, property_)
+                device_props[property_] = value
+
+            all_device_props[device] = device_props
+        return all_device_props
 
 
 if __name__ == "__main__":
     state_engine = DeviceProperties()
-    print(state_engine.get_current_stage_position())
-    print(state_engine.get_current_exposure_time())
-    print(state_engine.get_loaded_devices())
+    pprint(state_engine.get_all_properties())
 
-    print(state_engine.send_command("mmc.getSystemStateCache()"))
+    # this doesn't work, but should ideally work
