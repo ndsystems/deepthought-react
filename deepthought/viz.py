743154dc22215f80c21e73b444b60e90183be153
Kesavan Subburam <pskesavan@tifrh.res.in>
2020-05-19 02:14:18 +0530

    to prevent zoom error in mpld3

--- deepthought/viz.py
+++ deepthought/viz.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import mpld3
+from matplotlib_scalebar.scalebar import ScaleBar
 
 
 def imshow(image, size=(5, 5), dpi=150):
@@ -8,8 +9,10 @@ def imshow(image, size=(5, 5), dpi=150):
     ax = plt.Axes(fig, [0., 0., 1., 1.])
     ax.set_axis_off()
     fig.add_axes(ax)
+    scalebar = ScaleBar(0.2)
     plt.set_cmap('gray')
-    ax.imshow(image, aspect='equal')
+    ax.imshow(image, origin='lower', aspect='equal')
+    ax.add_artist(scalebar)
     return fig
 
 
